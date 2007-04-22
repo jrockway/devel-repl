f254750a4425f47eca1923f7e44b3b53b3c71104
epitaph <epitaph@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2007-04-22 13:46:23 +0000

    Added Plugin/NewlineHack, simple plugin to print a newline after output
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/trunk/Devel-REPL@3204 bd8105ee-0ff8-0310-8827-fb3f25b6796d

---
+++ lib/Devel/REPL/Plugin/NewlineHack.pm
@@ -0,0 +1,19 @@
+# Adds a newline after print. Some readlines need it some don't. I guess
+# we should clarify whether this is a bug and if so whose bug at some point
+# but this'll do for now ;)
+
+package Devel::REPL::Plugin::NewlineHack;
+
+use Moose::Role;
+
+use Data::Dumper;
+
+after 'print' => sub {
+  # not fussed about args
+  my ($self) = @_;
+  my $fh = $self->out_fh;
+  print $fh "\n";
+};
+
+1;
+
