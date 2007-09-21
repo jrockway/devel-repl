91d1e9ff20f3a2410ff5a683ac7c3088f40e1e0f
Sartak <Sartak@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2007-09-21 00:26:33 +0000

    Add MultiLine::PPI to the default profile <@mst> I'm willing to have it in the default profile :)
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/trunk/Devel-REPL@3763 bd8105ee-0ff8-0310-8827-fb3f25b6796d

--- lib/Devel/REPL/Profile/Default.pm
+++ lib/Devel/REPL/Profile/Default.pm
@@ -6,7 +6,7 @@ use namespace::clean -except => [ 'meta' ];
 with 'Devel::REPL::Profile';
 
 sub plugins {
-  qw(History LexEnv DDS Packages Commands);
+  qw(History LexEnv DDS Packages Commands MultiLine::PPI);
 }
 
 sub apply_profile {
