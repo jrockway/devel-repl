19ba909cb312f706f9275b61c55367298e031fce
nothingmuch <nothingmuch@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2008-05-06 07:49:51 +0000

    Devel::REPL::Error
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/trunk/Devel-REPL@4341 bd8105ee-0ff8-0310-8827-fb3f25b6796d

---
+++ lib/Devel/REPL/Error.pm
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+package Devel::REPL::Error;
+use Moose;
+
+# FIXME get nothingmuch to refactor and release his useful error object
+
+has type => (
+  isa => "Str",
+  is  => "ro",
+  required => 1,
+);
+
+has message => (
+  isa => "Str",
+  is  => "ro",
+  required => 1,
+);
+
+sub stringify {
+  my $self = shift;
+
+  sprintf "%s: %s", $self->type, $self->message;
+}
+__PACKAGE__
+
+__END__
