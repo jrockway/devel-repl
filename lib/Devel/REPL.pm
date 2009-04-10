79697f6184b05e3778e6090166d5bd9fcbd243b9
oliver <oliver@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2009-04-10 08:32:01 +0000

    had to cut a new release, missing some M::I files (how?) from the previous
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/trunk/Devel-REPL@5883 bd8105ee-0ff8-0310-8827-fb3f25b6796d

--- lib/Devel/REPL.pm
+++ lib/Devel/REPL.pm
@@ -5,7 +5,7 @@ use Moose;
 use namespace::clean -except => [ 'meta' ];
 use 5.008001; # backwards compat, doesn't warn like 5.8.1
 
-our $VERSION = '1.003005'; # 1.3.5
+our $VERSION = '1.003006'; # 1.3.6
 
 with 'MooseX::Object::Pluggable';
 
