67a934781bbbb30cfc24729ab642e6386bc469c5
Chris Marshall <devel.chm.01@gmail.com>
2010-06-09 21:59:34 -0400

    Update version to 1.003010_01 for development

--- lib/Devel/REPL.pm
+++ lib/Devel/REPL.pm
@@ -5,7 +5,7 @@ use Moose;
 use namespace::clean -except => [ 'meta' ];
 use 5.008001; # backwards compat, doesn't warn like 5.8.1
 
-our $VERSION = '1.003010';
+our $VERSION = '1.003010_01';
 
 with 'MooseX::Object::Pluggable';
 
