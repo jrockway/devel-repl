52d6b6af147beccedca56cb715343c16935e8078
Jesse Luehrs <doy@tozt.net>
2010-09-27 14:44:26 -0500

    version bump

--- lib/Devel/REPL.pm
+++ lib/Devel/REPL.pm
@@ -5,7 +5,7 @@ use Moose;
 use namespace::clean -except => [ 'meta' ];
 use 5.008001; # backwards compat, doesn't warn like 5.8.1
 
-our $VERSION = '1.003010_02';
+our $VERSION = '1.003012';
 
 with 'MooseX::Object::Pluggable';
 
