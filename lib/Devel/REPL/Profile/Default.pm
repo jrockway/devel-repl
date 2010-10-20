3d83c68dd2567b321a609cc1140f56f7a4a81fd4
Jonathan Rockway <jon@jrock.us>
2010-10-20 13:49:50 -0500

    enable resultnames/installresult by default

--- lib/Devel/REPL/Profile/Default.pm
+++ lib/Devel/REPL/Profile/Default.pm
@@ -6,7 +6,7 @@ use namespace::clean -except => [ 'meta' ];
 with 'Devel::REPL::Profile';
 
 sub plugins {
-  qw(History LexEnv DDS Packages Commands MultiLine::PPI);
+  qw(History LexEnv DDS Packages Commands MultiLine::PPI ResultNames InstallResult);
 }
 
 sub apply_profile {
